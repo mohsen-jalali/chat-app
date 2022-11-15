e03cdf79d768f6658e33f4604e731bf1a5d383e9
Mohsen Jalali <mohsen.jalali@webjar.ir>
2022-11-15 15:24:53 +0330

    feat: add app bar custom widget

---
+++ lib/src/presentation/common_widgets/app_bar.dart
@@ -0,0 +1,53 @@
+import 'package:chat_app/src/presentation/common_widgets/icon_button.dart';
+import 'package:chat_app/src/presentation/common_widgets/text_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CustomAppbar extends StatelessWidget implements PreferredSize {
+  final String? title;
+  final TextStyle? titleStyle;
+  final bool hasBackButton;
+  final bool centerTitle;
+  final Widget? leading;
+  final List<Widget>? actions;
+  final VoidCallback? onPressBack;
+
+  const CustomAppbar({
+    Key? key,
+    this.title,
+    this.titleStyle,
+    this.leading,
+    this.actions,
+    this.hasBackButton = false,
+    this.centerTitle = true,
+    this.onPressBack,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: TextWidget.bold(
+        title ?? "",
+        context: context,
+        additionalStyle: titleStyle,
+      ),
+      centerTitle: centerTitle,
+      actions: actions,
+      backgroundColor: Theme.of(context).colorScheme.background,
+      leading: Visibility(
+        visible: hasBackButton,
+        replacement: leading ?? const SizedBox.shrink(),
+        child: IconButtonWidget(
+          iconData: Icons.arrow_back_ios_new,
+          onPressed: onPressBack,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget get child => const SizedBox();
+
+  @override
+  Size get preferredSize => Size.fromHeight(64.h);
+}
